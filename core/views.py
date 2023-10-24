4de0c868ffc897b10680f66923c7bfc2df3735c4
Oluwatobi Ekundayo <tobiekundayo07@gmail.com>
2023-10-24 23:09:04 +0100

    added database tables and models

--- core/views.py
+++ core/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.template.loader import render_to_string, get_template
+from django.core.mail import EmailMessage
+from django.contrib.auth.decorators import *
+from voteguard import settings
+
+"""
+For each bug and feature I either solve or create, I'll drop a dad joke.
+
+1. What do you call a cow with no legs???
+    Ground Beef(solved user auth bug)
+
+2. Why are educated so hot???
+    Cause they have more degress(Fixed mailing list bugs)
+
+3. Why did the programmer need new glasses?
+    Becaue he couldn't C# *ba dum tsss (Fixed Looping Issue With Maps API)
+
+4. Why was 6 afraid of 7?
+    Because 7, 8, 9(Fixed styling issue with maps)
+
+5. What do you call a cow with no legs?
+    Ground BEEF!!!!!!
+"""
 
 
 def index(request):
